d018aba232ad472d1014c1c28d772140cfef1ee3
ThanhNhiet <nhiethiz@gmail.com>
2024-09-29 22:21:43 +0700

    add hardeware back press

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { BackHandler, View } from 'react-native';
 import LaunchScreen from '../pages/LaunchScreen';
 import Home from '../pages/Home';
 
@@ -13,6 +13,20 @@ export default function App() {
   const navigateToLaunchScreen = () => {
     setCurrentScreen('LaunchScreen');
   };
+
+  useEffect(() => {
+    const backAction = () => {
+      if (currentScreen === 'Home') {
+        navigateToLaunchScreen();
+        return true; // Ngăn chặn hành động mặc định của nút quay lại
+      }
+      return false; // Cho phép hành động mặc định nếu không ở màn hình Home
+    };
+
+    const backHandler = BackHandler.addEventListener('hardwareBackPress', backAction);
+
+    return () => backHandler.remove(); // Dọn dẹp listener khi component bị gỡ bỏ
+  }, [currentScreen]);
 
   return (
     <View style={{ flex: 1 }}>
