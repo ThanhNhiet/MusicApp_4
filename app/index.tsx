63a9650bcb54d96a7d90be56627448ff7cf5971f
PhamLeThanhNhiet <nhiethiz@gmail.com>
2024-10-30 18:07:55 +0700

    update index

--- app/index.tsx
+++ app/index.tsx
@@ -6,10 +6,12 @@ import Home from '../pages/HomeScreen';
 import TestNav from '../pages/TestNavigation';
 import PlaylistDetail from '../pages/PlaylistDetail';
 import PlayAudioScreen from '../pages/PlayAudioScreen';
+import ArtistProfile from '../pages/ArtistProfileScreen';
 
 export default function App() {
   const [currentScreen, setCurrentScreen] = useState('LaunchScreen');
   const [selectedSong, setSelectedSong] = useState(null);
+  const [idArtist, setIdArtist] = useState<string | null>(null);
 
   const navigateToHome = () => {
     setCurrentScreen('Home');
@@ -30,6 +32,11 @@ export default function App() {
   const navigateToPlayAudio = (song: any) => {
     setCurrentScreen('PlayAudioScreen');
     setSelectedSong(song); // Lưu bài hát hiện tại để truyền qua màn hình PlayAudioScreen
+  };
+
+  const navigateToArtistProfile = (id: string) => {
+    setCurrentScreen('ArtistProfile');
+    setIdArtist(id);
   };
 
   //back hardware button
@@ -53,19 +60,23 @@ export default function App() {
         set currentScreen là Home và render màn hình HomeScreen.tsx*/}
       {currentScreen === 'LaunchScreen' && <LaunchScreen navigateToHome={navigateToHome} />}
 
-      {currentScreen === 'Home' && <Home navigateToPlayListDetail={navigateToPlaylistDetail} />}
+      {currentScreen === 'Home' && <Home navigateToPlayListDetail={navigateToPlaylistDetail} navigateToArtistProfile={navigateToArtistProfile}/>}
       {currentScreen === 'PlaylistDetail' && <PlaylistDetail navigateToHome={navigateToHome} navigateToPlayAudio={navigateToPlayAudio} />}
       {currentScreen === 'Search' && <TestNav />}
       {currentScreen === 'PlayAudioScreen' && <PlayAudioScreen navigateToPlayListDetail={navigateToPlaylistDetail} song={selectedSong} />}
+      {currentScreen === 'ArtistProfile' && idArtist && <ArtistProfile navigateToHome={navigateToHome} idArtist={idArtist}/>}
 
       {/* Các màn hình có tab bar menu */}
-      {(currentScreen === 'Home' || currentScreen === 'Search' || currentScreen === 'PlaylistDetail') && (
+      {(currentScreen === 'Home' || currentScreen === 'Search' || currentScreen === 'PlaylistDetail'
+        || currentScreen === 'ArtistProfile'
+      ) && (
         <TabBarMenu
           activeTab={currentScreen}
           onTabPress={(tabName) => {
             if (tabName === 'Home') {
               navigateToHome();
-            } else if (tabName === 'Search') {  {/* Test nút search trên tabName */}
+            } else if (tabName === 'Search') {
+              {/* Test nút search trên tabName */ }
               navigateToTestNav();
             }
           }}
