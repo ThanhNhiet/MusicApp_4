dd6d5bbfff47b7d2270815dda641d56ed2acafef
TranKeTuanVuong <tuanvuongpokap@gmail.com>
2024-11-10 17:12:28 +0700

    update SubscriptionPlansScreen

--- pages/SubscriptionPlans.tsx
+++ pages/SubscriptionPlans.tsx
@@ -68,7 +68,7 @@ const styles = StyleSheet.create({
   },
   contentImage: {
     width: '100%',       
-    height: 300,        
+    height: 485,        
     marginTop: 5,
     resizeMode: 'contain',
   },
